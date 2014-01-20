cad9da623c266cd0ba1120c9d9288f1a5d2a2e6c
Alexandre <alexandrehtb@gmail.com>
2014-01-20 20:54:55 -0200

    Make a basic User model with secure password.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 MyImdb::Application.routes.draw do
   root  'static_pages#home'
-  match '/about', to: 'static_pages#about', via: 'get'
+  match '/signup',  to: 'users#new',          via: 'get'
+  match '/about',   to: 'static_pages#about', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
